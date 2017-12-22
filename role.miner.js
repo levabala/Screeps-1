22e1daed32d3e0f301fa6ff11a1a2eb7ac19599b
Lev <levabala@ya.ru>
2017-12-22 16:45:14 +0300

    Update role.miner.js
    run -> control
    secondArg -> creep id by work type

--- role.miner.js
+++ role.miner.js
@@ -1,25 +1,28 @@
 var roleMiner = {
     
     /** @param {Creep} creep **/
-    run: function(creep) {
-        
-        if (creep.carry.energy < creep.carryCapacity) {
-            creep.memory.miner = true;
-        }
-        else {
-            creep.memory.miner = false;
-        }
-        if(creep.memory.miner){
-        
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            creep.drop(RESOURCE_ENERGY);
-        }
-    }    
+    control: function(creep, id) {        
+        var sources = creep.room.find(FIND_SOURCES);
+        var sourceId = id % sources.length;
+        if(creep.harvest(sources[sourceId]) == ERR_NOT_IN_RANGE) 
+            creep.moveTo(sources[sourceId]);        
+    }   
 };
 
 module.exports = roleMiner;
+
+/*
+var minerId = 0;
+var workerId = 0;
+for (var i in creeps){
+    switch (creep.memory.role){
+        case "miner":
+            controlMiner(creep, minerId)
+            minerId++;
+            break;
+        case "worker":
+            controlWorker(creep, minerId)
+            workerId++;
+            break;
+    }
+}*/
